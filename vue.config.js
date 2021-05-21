569108fdb671b157c9c0cab5036c3783ff191de1
Mattia Thiella <mattia.thiella@qualitas.it>
2021-05-21 16:57:25 +0200

    add manifest to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    name: 'Feed App',
+    themeColor: '#ff8f00',
+    msTileColor: '#ff8f00',
+    manifestPath: 'manifest.json',
+    workboxOptions: {
+      exclude: [/\.map$/, /_redirects/] //this fixed it.
+    }
+  }
 }
