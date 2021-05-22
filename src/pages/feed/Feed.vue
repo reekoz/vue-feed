35a6d4ec30e5d4928299abd32b3384d894785ee9
Mattia Thiella <mattia.thiella@qualitas.it>
2021-05-22 11:14:52 +0200

    fix feed layout

--- src/pages/feed/Feed.vue
+++ src/pages/feed/Feed.vue
@@ -3,15 +3,6 @@
     <v-dialog v-model="editDialog" persistent max-width="400">
       <edit-post></edit-post>
     </v-dialog>
-    <v-row v-if="loading" justify="center">
-      <v-skeleton-loader
-        v-for="n in 6"
-        :key="n"
-        class="mx-auto ml-1 mr-1"
-        width="400"
-        type="card"
-      ></v-skeleton-loader>
-    </v-row>
     <v-row justify="center">
       <v-col lg="2" md="3" sm="4" xs="12">
         <v-btn color="primary" block @click.stop="addPost">
@@ -21,6 +12,15 @@
     </v-row>
     <v-row justify="center">
       <v-pagination v-model="page" :length="pageLenght"></v-pagination>
+    </v-row>
+    <v-row v-if="loading" justify="center">
+      <v-skeleton-loader
+        v-for="n in postsPerPage"
+        :key="n"
+        class="mx-auto ml-1 mr-1"
+        width="400"
+        type="card"
+      ></v-skeleton-loader>
     </v-row>
     <v-row v-if="!loading && posts && posts.length > 0" class="post-list">
       <feed-post v-for="p in posts" :key="p._id" :post="p"></feed-post>
